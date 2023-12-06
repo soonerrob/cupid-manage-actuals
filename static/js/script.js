6437e454a43dbcfa635ea80c422e1341cc513fd9
soonerrob <soonerrob@gmail.com>
2023-12-05 21:08:32 -0600

    add clear form after Add Actual Submit

--- static/js/script.js
+++ static/js/script.js
@@ -11,3 +11,10 @@ $(document).ready(function () {
     $(".alert").alert("close");
   }, 3000);
 });
+
+document.body.addEventListener("htmx:afterOnLoad", function (evt) {
+  if (evt.detail.successful) {
+    // Assuming your form has an id 'myForm'
+    document.getElementById("add-form").reset();
+  }
+});
